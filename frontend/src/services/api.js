1a3daed450b29505acad8833bc6c4e1b88015ca5
Deepak Kumar <dipak10012004@gmail.com>
2026-04-26 09:37:25 +0530

    updated backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const API = "http://localhost:5000/api";
 
-const API = "https://your-backend.onrender.com/api";
+const API = "https://healthcare-backend-u7hv.onrender.com"
 
 export const getDiagnosis = (data) =>
   axios.post(`${API}/diagnosis`, data);
